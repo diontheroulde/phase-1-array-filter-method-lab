df97c8300b86ffeaf290f67255fd441a37b6b114
Dion Theroulde <dionjr105@gmail.com>
2021-05-25 20:53:56 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name){
+     const newArray = drivers.filter(string => string.toLowerCase() === name.toLowerCase())
+        return newArray
+}
+
+function fuzzyMatch(drivers, name){
+    const fuzzyArray = drivers.filter(string => string.charAt(0) === name.charAt(0)
+    )
+    return fuzzyArray
+}
+
+function matchName(drivers, name){
+    const  matchArray = drivers.filter(string => string.name === name)
+    return matchArray
+}
